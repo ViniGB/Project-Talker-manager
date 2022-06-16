b41525c786f5c28390c869b2335cc75f20c36e4c
Vinicius Gomes <viniciusemog@hotmail.com>
2022-06-16 14:51:16 -0300

    Req 01 completed

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const talkers = 'talker.json';
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+// Req 01
+app.get('/talker', (_req, res) => {
+  const data = fs.readFileSync(talkers, 'utf8');
+  if (!data) return res.status(200).json([]);
+  return res.status(200).json(JSON.parse(data));
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
